4150ce21e67c96a7c888cc5f4d7b02160ce2f159
Seatlbiza <al361639@uji.es>
2018-11-13 15:07:08 +0100

    holaxd
    he metido mis cosas en esta otra cosa.

---
+++ ANTICLICK/Assets/Scripts/recogerMonedas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recogerMonedas : MonoBehaviour {
+
+	public int cofre = 0;
+
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.tag == "Recolectable"){
+			cofre = cofre + 1;
+			Destroy(col.gameObject);
+		}
+	
+	}
+}
