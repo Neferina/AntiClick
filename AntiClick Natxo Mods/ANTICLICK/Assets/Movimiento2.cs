070e8709f84e5ebbaadd014fac45a5b9a49b71ac
Natxo58 <natxocpsafa@gmail.com>
2018-11-14 13:30:00 +0100

    Cambios en el movimiento
    he cambiado basicamente el script de movimiento

---
+++ AntiClick Natxo Mods/ANTICLICK/Assets/Movimiento2.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento2 : MonoBehaviour {
+
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    public float dashSpeed;
+    public float dashCD;
+    public float dashCDMax;
+
+    public int jumps;
+
+    // Use this for initialization
+    void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0) {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && dashCD == 0)
+        {
+            if (facingRight == true)
+            {
+                rb.velocity = Vector2.right * dashSpeed;
+                dashCD = dashCDMax;
+            }else if (facingRight == false)
+            {
+                rb.velocity = Vector2.left * dashSpeed;
+                dashCD = dashCDMax;
+            }
+
+        }
+
+
+    }
+
+    void Update()
+    {
+        if (dashCD > 0)
+        {
+            dashCD--;
+        }
+
+        if (isGrounded == true)
+        {
+            jumps = 1;
+        }
+    
+        if (Input.GetKeyDown(KeyCode.UpArrow) && jumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            jumps--;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+}
---
+++ AntiClick Natxo Mods/ANTICLICK/Assets/Scripts/HeroController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour {
+	public float speed = 10f;
+    public float maxSpeed = 2.5f;
+    public float maxDashSpeed = 8.0f;
+    public bool ground;
+    public bool right = true;
+	public float jumpForce = 2f;
+
+	public bool tocado = false; //detecta cuando es tocado por un enemigo (para cambiar el color y detectar damage)
+	
+	private Rigidbody2D rb2d;
+	private Animator anim;
+    private SpriteRenderer render;
+	private bool jump;
+    private bool dash;
+    public int dashCoolDown;
+	private int jumpDelay = 0; //Cuenta fotogramas antes de saltar para que coincida con la animación.
+    public int dashDelay = 0; //Cuenta fotogramas mientras dura el dash para volver a aplicar el límite de velocidad.
+    // Use this for initialization
+    void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+        render = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+		anim.SetBool("Ground", ground);
+		anim.SetFloat("VSpeed", rb2d.velocity.y);
+		
+		if(Input.GetKeyDown(KeyCode.UpArrow) && ground){
+			jump = true;
+		}
+        if (Input.GetKeyDown(KeyCode.Space) && dashCoolDown==0 && !dash)
+        {
+            dash = true;
+            dashDelay = 10;
+            dashCoolDown = 100;
+        }
+        if (dash || dashDelay>0){dashDelay--;}
+        if (jump && ground)
+        {
+		    jumpDelay++;
+		}
+		anim.SetBool("Jump", jump);
+        anim.SetBool("Dash", dash);
+        anim.SetInteger("DashDelay", dashDelay);
+        if (dashCoolDown > 0) { dashCoolDown--; render.color = new Vector4(render.color.r, render.color.g, render.color.b, 0.7f); }
+        else { render.color = new Vector4(render.color.r, render.color.g, render.color.b, 1.0f); }
+    }
+	
+	void FixedUpdate(){
+        float hSpeed = Input.GetAxisRaw("Horizontal");
+        rb2d.AddForce(Vector2.right * speed * hSpeed);
+
+
+		if (hSpeed > 0.1f) {
+            transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            right = true;
+        }
+		if (hSpeed < -0.1f) {
+            transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+            right = false;
+        }
+		
+		if(jumpDelay>9 && ground){
+			rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+			jump = false;
+			jumpDelay = 0;
+		}
+
+        if (dash)
+        {
+            if (right) { rb2d.AddForce(Vector2.right * jumpForce, ForceMode2D.Impulse); }
+            else { rb2d.AddForce(Vector2.left * jumpForce, ForceMode2D.Impulse); }
+            dash = false;
+        }
+        if (dashDelay == 0)
+        {
+            rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed), rb2d.velocity.y);
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -maxDashSpeed, maxDashSpeed), 0.0f);
+        }
+
+        if (Mathf.Abs(rb2d.velocity.y)>0.1){
+			//Debug.Log(rb2d.velocity.y);
+		}
+	}
+	
+	
+}
