de9271d3f4d52a33832de7f237c2a720bab3bc88
Seatlbiza <al361639@uji.es>
2018-11-20 02:45:03 +0100

    uwu
    Versión mejorada

---
+++ AntiClick/ANTICLICK/Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Metemos el scene manager
+
+public class GameManager : MonoBehaviour {
+
+    bool gameEnd = false;
+
+    public void EndGame()
+    {
+        gameEnd = true;
+
+        if (gameEnd)
+        {
+            gameEnd = false;
+            Debug.Log("Fin");
+            Restart();
+        }
+        
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
